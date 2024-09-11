354b2caf8037b3c77ee58fac7eab9ac68a414b5f
jitka-machalova <168266361+jitka-machalova@users.noreply.github.com>
2024-09-11 19:56:29 +0200

    Update test_calculator3.py
    Vložení kódu pro automatizovaný test.

--- test_calculator1-5.py/test_calculator3.py
+++ test_calculator1-5.py/test_calculator3.py
@@ -1,1 +1,27 @@
+from playwright.sync_api import sync_playwright
 
+def test_calculator():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        # Přejděte na stránku kalkulačky
+        page.goto("https://calculator-online.net/cs/")
+
+        # Čekání na načtení tlačítka cookies a kliknutí na přijetí
+        page.wait_for_selector('#accept-choices')  # Selektor pro tlačítko cookies
+        page.click('#accept-choices')  # Kliknutí na tlačítko cookies
+
+        # Příklad: testování sčítání 6÷2
+        page.click('span.bg-white:has-text("6")')  # Selektor pro číslo 6
+        page.click('span.grey_color:has-text("÷")')  # Selektor pro ÷
+        page.click('span.bg-white:has-text("2")')  # Selektor pro číslo 2
+        page.click('span.blue_btn:has-text("=")')  # Selektor pro =
+
+        result = page.text_content('#showOutput').replace('\xa0', '').strip()  # Nahrazení nezlomitelné mezery a odstranění bílých znaků
+        assert result == "3", f'Expected 3 but got {result}'
+
+        browser.close()
+
+if __name__ == "__main__":
+    test_calculator()
